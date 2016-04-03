4dd3906ec9a56f4415ac7d62e28c8fa641d2210e
koskot77 <koskot77@hotmail.com>
2016-04-03 15:55:03 +0200

    cross

--- src/correlator.cc
+++ src/correlator.cc
@@ -16,14 +16,20 @@
 
 size_t nCores = 1;
 
-// global copy of the pointer to input matrix
+// global copy of the pointer to the input matrix
 const double **source = 0;
 size_t nRows = 0, nColumns = 0;
-
 // global copy of the intermediate results (need to recompute when input changes)
 double *cachedMean = 0, *cachedSd = 0;
 float  *correlations = 0;        // use single precision to save some space
 unsigned long long *indices = 0; // keep in mind that addressing scales N^2
+
+// same for cross correlations between two different input matricies
+const double **source1 = 0, **source2 = 0;
+size_t nRows1 = 0, nColumns1 = 0;
+size_t nRows2 = 0, nColumns2 = 0;
+double *cachedMean1 = 0, *cachedSd1 = 0;
+double *cachedMean2 = 0, *cachedSd2 = 0;
 
 extern "C" {
 
@@ -32,6 +38,10 @@ void cleanUp(void){
     if( correlations ){ delete [] correlations; correlations = 0; }
     if( cachedMean   ){ delete [] cachedMean;   cachedMean   = 0; }
     if( cachedSd     ){ delete [] cachedSd;     cachedSd     = 0; }
+    if( cachedMean1  ){ delete [] cachedMean1;  cachedMean1  = 0; }
+    if( cachedSd1    ){ delete [] cachedSd1;    cachedSd1    = 0; }
+    if( cachedMean2  ){ delete [] cachedMean2;  cachedMean2  = 0; }
+    if( cachedSd2    ){ delete [] cachedSd2;    cachedSd2    = 0; }
     if( indices      ){ delete [] indices;      indices      = 0; }
 }
 void setCores(int *n){ nCores = *n; }
@@ -130,6 +140,142 @@ void clusterCorrelations(int *dim, double *inMatrix, double *cutoffDistance, int
 
     for(unsigned int row=0; row<nRows; row++)
         outClustering[row] = uf.findCluster(row+1);
+
+}
+
+void reclusterCorrelations(double *cutoffDistance, int *outClustering){
+    if( !source ){
+        printf("Clustering not yet done, nothing to recluster\n");
+        return;
+    }
+
+    UnionFind uf(nRows);
+    for(unsigned long long i=0; i<nRows*(nRows-1)/2; i++){
+        unsigned long long index = indices[i];
+        unsigned long row1  = (unsigned long)( (1+sqrt(1+8*index))/2 );
+        unsigned long row2  = index - row1*(row1-1)/2;
+        if( -correlations[i] < *cutoffDistance ) break;
+        int node1 = row1+1;
+        int node2 = row2+1;
+        int cluster1 = uf.findCluster(node1);
+        int cluster2 = uf.findCluster(node2);
+        if( cluster1 != cluster2 ) uf.joinClusters(cluster1,cluster2);
+    }
+
+    for(unsigned int row=0; row<nRows; row++)
+        outClustering[row] = uf.findCluster(row+1);
+}
+
+}
+
+void computeBlockOfCrossCorrelations(unsigned long long begin, unsigned long long end){
+    const double (*series1)[nColumns1] = (const double (*)[nColumns1])source1;
+    const double (*series2)[nColumns2] = (const double (*)[nColumns2])source2;
+    size_t length = nColumns1; // for now assumed to be = nCulumns2
+    for(unsigned long long index=begin; index<end; index++){
+
+        unsigned long row1  = (unsigned long)( index/nRows2 );
+        unsigned long row2  = index - row1*nRows2;
+
+        correlations[index] = -fabs( cor(series1[row1],cachedMean1[row1],cachedSd1[row1],
+                                         series2[row2],cachedMean2[row2],cachedSd2[row2], length ) );
+    }
+}
+
+void computeCrossCorrelationsAndSort(void){
+    cleanUp();
+
+    // allocate space for all-with-all pair correlations 
+    correlations        = new  float  [nRows1*nRows2];
+    bzero(correlations, sizeof(float) *nRows1*nRows2);
+
+    double (*series1)[nColumns1] = (double (*)[nColumns1])(source1);
+    double (*series2)[nColumns2] = (double (*)[nColumns2])(source2);
+
+    // caching means and sds
+    cachedMean1 = new double [nRows1];
+    cachedSd1   = new double [nRows1];
+    cachedMean2 = new double [nRows2];
+    cachedSd2   = new double [nRows2];
+    
+    for(unsigned int row=0; row<nRows1; row++){
+        cachedMean1[row] = mean(series1[row],nColumns1);
+        cachedSd1  [row] = sd  (series1[row],nColumns1);
+    }
+
+    for(unsigned int row=0; row<nRows2; row++){
+        cachedMean2[row] = mean(series2[row],nColumns2);
+        cachedSd2  [row] = sd  (series2[row],nColumns2);
+    }
+
+    // multicore calculations
+    printf("Correlating all in one with all in another\n");
+    const size_t maxNumThreads = nCores;
+    std::future<void> results [ maxNumThreads ];
+    const unsigned long long maxIndex  =  nRows1*nRows2;
+    const unsigned int       maxBlocks = (nRows1>100 && nRows2>100 ? 100 : 1);
+    for(unsigned int block=0; block<maxBlocks; block++){
+        unsigned long long begin =  block    * maxIndex / maxBlocks ;
+        unsigned long long   end = (block+1) * maxIndex / maxBlocks ;
+
+        // identify a free thread
+        size_t freeThread = 0;
+        for( ;  results[freeThread].valid() &&
+                results[freeThread].wait_for(std::chrono::milliseconds(100)) != std::future_status::ready ; )
+            if( freeThread == maxNumThreads-1 ) freeThread = 0; else freeThread++;
+
+        // submit
+        results[freeThread] = std::async(std::launch::async, computeBlockOfCrossCorrelations, begin, end);
+        printf("  finished  block %d/%d in thread %ld\n",block,maxBlocks,freeThread);
+    }
+    printf("  finalizing ... \n");
+
+    // wait until all threads finish
+    for(size_t thr=0; thr<maxNumThreads; thr++)
+        if( results[thr].valid() ) results[thr].wait();
+
+    /// Beginning of the sorting step
+
+    // keep track of original indices while sorting
+    indices = new unsigned long long [nRows1*nRows2];
+    for(unsigned long long i=0; i<nRows1*nRows2; i++) indices[i] = i;
+
+    printf("Sorting\n");
+    unsigned long long size = nRows1*nRows2;
+    splitQuickSort( correlations, indices, size );
+}
+
+extern "C" {
+
+void crossCorrelations(int *dim1, double *inMatrix1,int *dim2, double *inMatrix2, double *cutoffDistance, int *outGrouping){
+
+    if( source1  != (const double**)inMatrix1 || nRows1 != size_t(dim1[0]) || nColumns1 != size_t(dim1[1]) ||
+        source2  != (const double**)inMatrix2 || nRows2 != size_t(dim2[0]) || nColumns2 != size_t(dim2[1]) ){
+        source1   = (const double**)inMatrix1;
+        source2   = (const double**)inMatrix2;
+        nRows1   = dim1[0];
+        nColumns1= dim1[1];
+        nRows2   = dim2[0];
+        nColumns2= dim2[1];
+        if( nColumns1 != nColumns2 ){
+            printf("Windowing is not implemented\n");
+            return;
+        }
+        computeCrossCorrelationsAndSort();
+    }
+
+    int *outGrouping12 = outGrouping;
+    int *outGrouping21 = outGrouping + nRows1;
+
+    printf("Groping\n");
+    for(unsigned long long i=0; i<nRows1*nRows2; i++){
+        unsigned long long index = indices[i];
+        unsigned long row1  = (unsigned long)( index/nRows2 );
+        unsigned long row2  = index - row1*nRows2;
+        if( -correlations[i] < *cutoffDistance ) break;
+        outGrouping12[row1] = row2+1;
+        outGrouping21[row2] = row1+1;
+    }
 
 }
 
