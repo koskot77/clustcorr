c23040fd8e5b79b49ef77b91dedfe08419acf9cc
koskot77 <koskot77@hotmail.com>
2016-04-08 15:26:20 +0200

    unexpected aggregate behavior of avoiding lists for simple cases

--- R/cluster.correlations.R
+++ R/cluster.correlations.R
@@ -1,13 +1,15 @@
 cluster.correlations <- function(m,cutoff){
     d  <- dim(m);
     cl <- .C("clusterCorrelations",as.integer(d),as.numeric(t(m)),as.numeric(cutoff),clust=integer(d[1]))$clust
-    df <- data.frame( cl=cl )
-    df <- cbind(df,1:length(cl))
-    colnames(df) <- c("cl","ind")
-    clustering <- aggregate( df, by=list(Index=df$cl), function(x){x} )
-    bigfirst  <- c(1)
-    if( dim(clustering)[1] != 1 ){
-        bigfirst <- order( unlist( lapply(clustering[,3],length) ), decreasing = T )
+    if( length( unique(cl) ) > 1 ){
+        df <- data.frame( cl=cl )
+        df <- cbind(df,1:length(cl))
+        colnames(df) <- c("cl","ind")
+        clustering   <- aggregate( df, by=list(Index=df$cl), function(x){x} )
+        bigfirst     <- order( unlist( lapply(clustering[,3],length) ), decreasing = T )
+        return(clustering[bigfirst,3])
+    } else {
+        # if all were merged in one cluster
+        return(list('1'=list(cl)))
     }
-    return(clustering[bigfirst,3])
 }
--- R/recluster.correlations.R
+++ R/recluster.correlations.R
@@ -1,18 +1,21 @@
 recluster.correlations <- function(m,cutoff=NULL,nclust=NULL){
     if( !is.null(cutoff) ){
-        if( !is.null(nclust) )  print("Both 'cutoff' and 'nclust' arguments are provided, ignoring 'nclust'")
+        if( !is.null(nclust) ) print("Both 'cutoff' and 'nclust' arguments are provided, ignoring 'nclust'")
         cl <- .C("reclusterCorrelationsAtCutoff",as.numeric(cutoff),clust=integer(dim(m)[1]))$clust
     } else {
         if( is.null(nclust) ){ print("Neither 'cutoff' nor 'nclust' arguments are provided, returning NULL"); return(NULL) }
         cl <- .C("reclusterCorrelationsForNclust",as.integer(nclust),clust=integer(dim(m)[1]))$clust
     }
-    df <- data.frame( cl=cl )
-    df <- cbind(df,1:length(cl))
-    colnames(df) <- c("cl","ind")
-    clustering   <- aggregate( df, by=list(Index=df$cl), function(x){x} )
-    bigfirst     <- c(1)
-    if( dim(clustering)[1] != 1 ){
-        bigfirst <- order( unlist( lapply(clustering[,3],length) ), decreasing = T )
+    if( length( unique(cl) ) > 1 ){
+        df <- data.frame( cl=cl )
+        df <- cbind(df,1:length(cl))
+        colnames(df) <- c("cl","ind")
+        clustering   <- aggregate( df, by=list(Index=df$cl), function(x){x} )
+        bigfirst     <- order( unlist( lapply(clustering[,3],length) ), decreasing = T )
+        return(clustering[bigfirst,3])
+    } else {
+        # if all were merged in one cluster
+        return(list('1'=list(cl)))
     }
-    return(clustering[bigfirst,3])
 }
+
